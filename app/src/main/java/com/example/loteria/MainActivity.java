d88bac4ffeba976984036e78576f1567d82318e0
iovannaEscarceega10 <iovassoto@gmail.com>
2019-02-06 08:27:49 -0600

    Loteria

---
+++ app/src/main/java/com/example/loteria/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.loteria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13, i14, i15, i16;
+
+    int c;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        i1 = findViewById(R.id.i1);
+        i2 = findViewById(R.id.i2);
+        i3 = findViewById(R.id.i3);
+        i4 = findViewById(R.id.i4);
+        i5 = findViewById(R.id.i5);
+        i6 = findViewById(R.id.i6);
+        i7 = findViewById(R.id.i7);
+        i8 = findViewById(R.id.i8);
+        i9 = findViewById(R.id.i9);
+        i10 = findViewById(R.id.i10);
+        i11 = findViewById(R.id.i11);
+        i12 = findViewById(R.id.i12);
+        i13 = findViewById(R.id.i13);
+        i14 = findViewById(R.id.i14);
+        i15 = findViewById(R.id.i15);
+        i16 = findViewById(R.id.i16);
+
+
+        i1.setOnClickListener(this);
+        i2.setOnClickListener(this);
+        i3.setOnClickListener(this);
+        i4.setOnClickListener(this);
+        i5.setOnClickListener(this);
+        i6.setOnClickListener(this);
+        i7.setOnClickListener(this);
+        i8.setOnClickListener(this);
+        i9.setOnClickListener(this);
+        i10.setOnClickListener(this);
+        i11.setOnClickListener(this);
+        i12.setOnClickListener(this);
+        i13.setOnClickListener(this);
+        i14.setOnClickListener(this);
+        i15.setOnClickListener(this);
+        i16.setOnClickListener(this);
+
+
+    }
+    @Override
+    public void onClick(View v) {
+        c++;
+        v.setBackgroundResource(R.drawable.frijol);
+    }
+
+
+}
