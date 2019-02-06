faa21b18fd7f6568ac0dafac886bfd2ffaccad34
iovannaEscarceega10 <iovassoto@gmail.com>
2019-02-06 13:13:41 -0600

    Loteria insertando puntito

--- app/src/main/java/com/example/loteria/MainActivity.java
+++ app/src/main/java/com/example/loteria/MainActivity.java
@@ -6,11 +6,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    ImageButton i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13, i14, i15, i16;
-
+    Button i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13, i14, i15, i16;
+    TextView txt1;
     int c;
 
     @Override
@@ -34,7 +35,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         i14 = findViewById(R.id.i14);
         i15 = findViewById(R.id.i15);
         i16 = findViewById(R.id.i16);
-
+        txt1 = findViewById(R.id.txt1);
 
         i1.setOnClickListener(this);
         i2.setOnClickListener(this);
@@ -58,8 +59,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         c++;
-        v.setBackgroundResource(R.drawable.frijol);
+        switch (v.getId())
+        {
+
+            case R.id.i1:
+                i1.setText("●");break;
+            case R.id.i2:
+                i2.setText("●");break;
+            case R.id.i3:
+                i3.setText("●");break;
+            case R.id.i4:
+                i4.setText("●");break;
+            case R.id.i5:
+                i5.setText("●");break;
+            case R.id.i6:
+                i6.setText("●");break;
+            case R.id.i7:
+                i7.setText("●");break;
+            case R.id.i8:
+                i8.setText("●");break;
+            case R.id.i9:
+                i9.setText("●");break;
+            case R.id.i10:
+                i10.setText("●");break;
+            case R.id.i12:
+                i12.setText("●");break;
+            case R.id.i11:
+                i11.setText("●");break;
+            case R.id.i13:
+                i13.setText("●");break;
+            case R.id.i14:
+                i14.setText("●");break;
+            case R.id.i15:
+                i15.setText("●");break;
+            case R.id.i16:
+                i16.setText("●");break;
+
+        }
     }
+    
 
 
 }
